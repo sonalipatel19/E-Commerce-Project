aef0661647cfce404f65924572671393a4443e9e
sonalipatel19 <sonali@gmail.com>
2025-12-22 11:18:32 -0500

    creat new environment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@
   required_version = ">= 1.5.0"
 
   backend "azurerm" {
-    resource_group_name   = "demo-rg1"       # Resource group containing storage account
+    resource_group_name   = "tfstate-rg"       # Resource group containing storage account
     storage_account_name  = "storage12b"   # Storage account name
     container_name        = "terraform"          # Blob container name
     key                   = "terraform.tfstate" # State file name
